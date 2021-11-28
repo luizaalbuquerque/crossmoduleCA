bb5c23da382f18e697799a99a38d7bf989cf40d3
luizaalbuquerque <72030187+luizaalbuquerque@users.noreply.github.com>
2021-11-28 00:03:51 +0000

    EditingAdmin

--- src/crossmoduleca/Login.java
+++ src/crossmoduleca/Login.java
@@ -20,32 +20,140 @@ public class Login {
         Connection conn;
         PreparedStatement pstm = null;
 
-        String query = "SELECT * FROM users";
+        //String query = "SELECT * FROM users WHERE username = '" + username + "'";
+        String query = "SELECT * FROM USERS WHERE username = '" + username + "' AND pswd = '" + pswd + "'";
+        // String adminQuery = "SELECT * FROM USERS WHERE USERTYPE = ADMIN;
 
         conn = new MySQLConection().dbconn();
         ResultSet rs = null;
-        ResultSet rscheck = null;
-        
-        
-         try {
+        ResultSet allUsers = null;
+        Statement stmt = null;
+
+        try {
             // create a connection
-            Statement stmt = conn.createStatement();
+            stmt = conn.createStatement();
             //execute the query 
-            stmt.executeUpdate(query);
+            rs = stmt.executeQuery(query);
+//
+
+            if (rs.next()) {
+
+                String fullname = rs.getString("fullname");
+                String myID =  rs.getString("id");
+                
+                System.out.println("Welcome " + fullname);
+                System.out.println("\n");
+
+                String userType = rs.getString("usertype");
+                if (userType.equals("admin")) {
+
+                    allUsers = stmt.executeQuery("SELECT * FROM USERS");
+
+                    while (allUsers.next()) {
+                        String id = allUsers.getString("id");
+                        String userFullName = allUsers.getString("fullname");
+                        System.out.println(id + " - " + userFullName);
+
+                    }
+                    System.out.println("Choose one option: 1 - Edit your profile or 2 for delete user");
+                    Scanner scanner = new Scanner(System.in);
+                    String option = scanner.next();
+                    if (option.equals("1")) {
+                        System.out.println("I want edit my profile");
+                       
+                        if (scanner.next().equals("yes")) {
+                            //codigo
+                             Scanner scannerUser = new Scanner(System.in);
+
+                System.out.print("Username: "); //check username
+
+                String user = scannerUser.next().toLowerCase();
+
+                System.out.print("Password: "); //check password
+
+                String pwd = scannerUser.next().toLowerCase();
+
+                System.out.print("Gender: "); //check gender
+
+                String gender = scannerUser.next().toLowerCase();
+                System.out.print("Usertype: "); //check usertype
+
+                String usertype = scannerUser.next().toLowerCase();
+
+                System.out.print("Fullname: "); //check fullname
+
+                String userfullname = scannerUser.next().toLowerCase();
+                
+                 String q = "UPDATE USERS SET fullname = '" + userfullname + "', pswd = '" + pwd + "', gender = '" + gender + "', username = '" + user + "', usertype = '" + usertype + "' where id = " + myID;
+                 stmt.executeUpdate(q);
+                            
+                            
+                            
+                        }else{
+                           //
+                        }
+                        
+                        
+                        
+                        
+                        
+                        
+                    } else if (option.equals("2")) {
+                        System.out.println("I wanna delete user");
+                        System.out.println("Choose an user by ID:");
+                        
+                        String q = "DELETE FROM USERS where id = " + scanner.next();
+                        stmt.executeUpdate(q);
+                       
+                    }
+
+                    allUsers.close();
+
+                }
+
+            } else {
+
+                System.out.println("User not founded. Try again! Or type 0 to finish");
+
+                System.out.println("would you like to create a new account? ");
+
+                Scanner scannerUser = new Scanner(System.in);
+
+                System.out.print("Username: "); //check username
+
+                String user = scannerUser.next().toLowerCase();
+
+                System.out.print("Password: "); //check password
+
+                String pwd = scannerUser.next().toLowerCase();
+
+                System.out.print("Gender: "); //check gender
+
+                String gender = scannerUser.next().toLowerCase();
+                System.out.print("Usertype: "); //check usertype
+
+                String usertype = scannerUser.next().toLowerCase();
+
+                System.out.print("Fullname: "); //check fullname
+
+                String fullname = scannerUser.next().toLowerCase();
+
+                SingUp signup = new SingUp();
+                signup.NewAccountWith(fullname, user, pwd, usertype, gender);
+
+//         
+            }
 
         } catch (SQLException e) {
             System.out.println("error");
+            e.printStackTrace();
         }
+        stmt.close();
 
-//        try {
-//            pstm = conn.prepareStatement(query);
-//            rscheck = pstm.executeQuery(query);
-//            pstm = conn.prepareStatement(query);
-//            rs = pstm.executeQuery(query);
-//
-//            if (rscheck.next()) { // checking username
-//
-//                while (rs.next()) {
+        rs.close();
+        conn.close();
+    }
+
 //
 //                    pswd = rs.getString("pswd");
 //
@@ -62,51 +170,10 @@ public class Login {
 //
 //                        // Close the result set, statement and the connection
 //                        rs.close();
-//                        rscheck.close();
-//                        pstm.close();
-//                        conn.close();
-//                    }
-//                }
-//
-//            } else {
-////error output for user 
-//                System.out.println("User not founded. Try again! Or type 0 to finish");
-//
-//                System.out.println("would you like to create a new account? ");
-//
-//                Scanner scannerUser = new Scanner(System.in);
-//
-//                System.out.print("Username: "); //check username
-//
-//                String user = scannerUser.next().toLowerCase();
-//
-//                System.out.print("Password: "); //check password
-//
-//                String pwd = scannerUser.next().toLowerCase();
-//
-//                System.out.print("Gender: "); //check gender
-//
-//                String gender = scannerUser.next().toLowerCase();
-//                System.out.print("Usertype: "); //check usertype
-//
-//                String usertype = scannerUser.next().toLowerCase();
-//
-//                System.out.print("Fullname: "); //check fullname
-//
-//                String fullname = scannerUser.next().toLowerCase();
-//
-//                SingUp signup = new SingUp();
-//                signup.NewAccountWith(fullname, user, pwd, usertype, gender);
-//
-//                //scannerUsername.next().toLowerCase();
-//                // CrossmoduleCA.login();
-//            }
 //        } catch (SQLException e) {
 //            System.out.print("Not supported yet.");
 //        }
-
-    }
-
+//    }
 //    static void login(String username, String pswd) {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 //    }
