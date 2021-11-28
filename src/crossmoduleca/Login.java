baa37c0673507616197ff821b22848a4d772893e
luizaalbuquerque <72030187+luizaalbuquerque@users.noreply.github.com>
2021-11-28 01:02:18 +0000

    givingAdminAccessToTheTable

--- src/crossmoduleca/Login.java
+++ src/crossmoduleca/Login.java
@@ -9,102 +9,110 @@ import java.util.Scanner;
  */
 public class Login {
 
+//    variables
     public static String pswdinput;
     public static String username;
 
     public static void loginCheck(String username, String pswd) throws SQLException {
 
         pswdinput = pswd;
-//        username =usern;
-
         Connection conn;
-        PreparedStatement pstm = null;
+//        PreparedStatement pstm = null;
 
-        //String query = "SELECT * FROM users WHERE username = '" + username + "'";
+//        checking username and password
         String query = "SELECT * FROM USERS WHERE username = '" + username + "' AND pswd = '" + pswd + "'";
-        // String adminQuery = "SELECT * FROM USERS WHERE USERTYPE = ADMIN;
 
+//        variables 
         conn = new MySQLConection().dbconn();
         ResultSet rs = null;
         ResultSet allUsers = null;
         Statement stmt = null;
 
         try {
+
             // create a connection
             stmt = conn.createStatement();
+
             //execute the query 
             rs = stmt.executeQuery(query);
-//
 
+//            checking boolean ResultSet, if it is true, do the folling: 
             if (rs.next()) {
 
+//                assigning variables full name and id 
                 String fullname = rs.getString("fullname");
-                String myID =  rs.getString("id");
-                
+                String myID = rs.getString("id");
+
+//                testing
                 System.out.println("Welcome " + fullname);
                 System.out.println("\n");
 
+//                checking if the user type is admin 
                 String userType = rs.getString("usertype");
                 if (userType.equals("admin")) {
 
+//                   select all table 
                     allUsers = stmt.executeQuery("SELECT * FROM USERS");
+
+//          show all table in the DataBase, using the while loop to print all rows
+                    System.out.println("\nID " + "|" + "\t" + "|" + "FULL NAME" + "|" + "\t\t" + "|" + "USERNAME" + "|" + "\t\t" + "|" + "GENDER" + "|" + " \t\t" + "|" + "PASSWORD" + "|" + "\t" + "|" + "ACTIVE" + "|" + "\t" + "|" + "TYPE");
 
                     while (allUsers.next()) {
                         String id = allUsers.getString("id");
                         String userFullName = allUsers.getString("fullname");
-                        System.out.println(id + " - " + userFullName);
+                        String showusername = allUsers.getString("username");
+                        String pwd = allUsers.getString("pswd");
+                        String usertype = allUsers.getString("usertype");
+                        String gender = allUsers.getString("gender");
+                        String active = allUsers.getString("active");
+                        System.out.println("----------------------------------------------------------------------------------------------------------------------------");
+                        System.out.println(id + "|" + "\t" + "|" + userFullName + "|" + "\t\t" + "|" + showusername + "|" + "\t\t" + "|" + gender + "|" + " \t\t\t" + "|" + pwd + "|" + "\t" + "|" + active + "|" + "\t\t" + "|" + usertype + "\n");
 
                     }
-                    System.out.println("Choose one option: 1 - Edit your profile or 2 for delete user");
+                    
+                    System.out.println("Choose one option: " + "\n" + "1 - Edit your profile" + " \n" + "2 - Delete an user");
                     Scanner scanner = new Scanner(System.in);
                     String option = scanner.next();
                     if (option.equals("1")) {
                         System.out.println("I want edit my profile");
-                       
+
                         if (scanner.next().equals("yes")) {
-                            //codigo
-                             Scanner scannerUser = new Scanner(System.in);
 
-                System.out.print("Username: "); //check username
+                            Scanner scannerUser = new Scanner(System.in);
 
-                String user = scannerUser.next().toLowerCase();
+                            System.out.print("Username: "); //check username
 
-                System.out.print("Password: "); //check password
+                            String user = scannerUser.next().toLowerCase();
 
-                String pwd = scannerUser.next().toLowerCase();
+                            System.out.print("Password: "); //check password
 
-                System.out.print("Gender: "); //check gender
+                            String pwd = scannerUser.next().toLowerCase();
 
-                String gender = scannerUser.next().toLowerCase();
-                System.out.print("Usertype: "); //check usertype
+                            System.out.print("Gender: "); //check gender
 
-                String usertype = scannerUser.next().toLowerCase();
+                            String gender = scannerUser.next().toLowerCase();
+                            System.out.print("Usertype: "); //check usertype
 
-                System.out.print("Fullname: "); //check fullname
+                            String usertype = scannerUser.next().toLowerCase();
 
-                String userfullname = scannerUser.next().toLowerCase();
-                
-                 String q = "UPDATE USERS SET fullname = '" + userfullname + "', pswd = '" + pwd + "', gender = '" + gender + "', username = '" + user + "', usertype = '" + usertype + "' where id = " + myID;
-                 stmt.executeUpdate(q);
-                            
-                            
-                            
-                        }else{
-                           //
+                            System.out.print("Fullname: "); //check fullname
+
+                            String userfullname = scannerUser.next().toLowerCase();
+
+                            String q = "UPDATE USERS SET fullname = '" + userfullname + "', pswd = '" + pwd + "', gender = '" + gender + "', username = '" + user + "', usertype = '" + usertype + "' where id = " + myID;
+                            stmt.executeUpdate(q);
+
+                        } else {
+                            //
                         }
-                        
-                        
-                        
-                        
-                        
-                        
+
                     } else if (option.equals("2")) {
                         System.out.println("I wanna delete user");
                         System.out.println("Choose an user by ID:");
-                        
+
                         String q = "DELETE FROM USERS where id = " + scanner.next();
                         stmt.executeUpdate(q);
-                       
+
                     }
 
                     allUsers.close();
