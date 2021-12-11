3048c741141975c62ad6aa3369fe78f973c417ac
luizaalbuquerque <72030187+luizaalbuquerque@users.noreply.github.com>
2021-12-11 18:02:34 +0000

    linearAlgebraThinking

--- src/crossmoduleca/LinearEquations.java
+++ src/crossmoduleca/LinearEquations.java
@@ -5,10 +5,66 @@
  */
 package crossmoduleca;
 
+import java.util.Scanner;
+
 /**
  *
  * @author luizaalbuquerque
  */
 public class LinearEquations {
+
+    public static void main (String arg[]){
+
+       
+
+        System.out.println("----- 3 variable linear euqation-----");
+
+         Scanner userInput = new Scanner(System.in);
+         
+            char[] variable = {'x', 'y', 'z'};
+            
+            System.out.println("Enter the coefficient of 3 variables  \n in the format show: \n ax + by+ cz = d");
+  
+    int [][] matrix = new int [3] [3];
+    int [][] constant = new int [3][1];
+    
+    for (int i=0; i<3; i++){
+        for (int j =0; j<3; j++){
+    matrix [i][j] = userInput.nextInt();
+    
+    
+    
+    
+            
+        }
+        
+        constant [i][0] =userInput.nextInt();
+        
+            }
+     System.out.println(
+            "Matrix representation of above linear equations is: ");
+ 
+        // Outer loop for iterating rows
+        for (int i = 0; i < 3; i++) {
+ 
+            // Inner loop for iterating columns
+            for (int j = 0; j < 3; j++) {
+ 
+                System.out.print(" " + matrix[i][j]);
+            }
+ 
+            System.out.print("  " + variable[i]);
+            System.out.print("  =  " + constant[i][0]);
+            System.out.println();
+        }
+ 
+       
     
 }
+    
+    
+   
+    
+    }
+
+
