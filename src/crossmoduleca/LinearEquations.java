0cea1b024e51a36d4026079aebf9c285f8ee8eae
luizaalbuquerque <72030187+luizaalbuquerque@users.noreply.github.com>
2021-12-14 22:36:03 +0000

    retriving2.0

--- src/crossmoduleca/LinearEquations.java
+++ src/crossmoduleca/LinearEquations.java
@@ -6,7 +6,7 @@ import java.sql.*;
 
 public class LinearEquations {
 
-<<<<<<< HEAD
+
     public static void main(String[] args) throws SQLException {
 
 //           Connection conn;
@@ -176,35 +176,5 @@ int number =Integer.parseInt(JOptionPane.showInputDialog(null,"Enter the coeffic
     }
 }
         
-=======
-   public static void main(String[] args) throws SQLException {
-    
-   }
+
    
-public static void solveLinearEquations(double variable1, double variable2, double variable3, double variable4, double variable5, double variable6) {
- Scanner option = new Scanner(System.in);
-//    int x = option.nextInt():
-    
-//       String calculate = JOptionPane.showInputDialog("calculate");
-               
-  if ( option.nextInt()==2) {
-      variable1 = Integer.parseInt(JOptionPane.showInputDialog(null, "a"));
-      variable2 = Integer.parseInt(JOptionPane.showInputDialog(null, "b"));
-      variable3 = Integer.parseInt(JOptionPane.showInputDialog(null, "c"));
-      variable4 = Integer.parseInt(JOptionPane.showInputDialog(null, "d"));
-      variable5 = Integer.parseInt(JOptionPane.showInputDialog(null, "e"));
-      variable6 = Integer.parseInt(JOptionPane.showInputDialog(null, "f"));
-      
-//     
-      double det = ((variable1) * (variable4) - (variable2) * (variable3));
-      double x = ((variable4) * (variable5) - (variable2) * (variable6)) / det;
-      double y = ((variable1) * (variable6) - (variable3) * (variable5)) / det;
-      System.out.print("x=" + x + " y=" + y);
- 
-  
-  
-      System.out.println(det);
-  } else {
-  }}
-}
->>>>>>> parent of 34bf7e7 (Merge branch 'main' of https://github.com/luizaalbuquerque/crossmoduleCA)
